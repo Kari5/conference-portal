5690f6f933c20e68a9fac3ec1414ff52e01ea4bc
winkari5@gmail.com <winkari5@gmail.com@3c564a0e-377f-4616-955d-181d525d466c>
2012-10-20 12:44:11 +0000

    Security config és egyebek.

--- trunk/ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
+++ trunk/ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
@@ -1,23 +1,22 @@
 package hu.bme.dtt.conferenceportal.session;
 
+import org.jboss.logging.Logger;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.log.Log;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
 
 @Name("authenticator")
 public class Authenticator
 {
-    @Logger private Log log;
+    private static final Logger LOGGER = Logger.getLogger(Authenticator.class);
 
     @In Identity identity;
     @In Credentials credentials;
 
     public boolean authenticate()
     {
-        log.info("authenticating {0}", credentials.getUsername());
+    	LOGGER.info("authenticating " + credentials.getUsername());
         //write your authentication logic here,
         //return true if the authentication was
         //successful, false otherwise
