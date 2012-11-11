af66866b2614d85ada669a7d8fa78b3f8320e676
winkari5@gmail.com <winkari5@gmail.com@3c564a0e-377f-4616-955d-181d525d466c>
2012-11-11 20:13:16 +0000

    hashCode és equals függvények

--- trunk/DataAccess-ejb/ejbModule/hu/bme/dtt/conferenceportal/entity/Tag.java
+++ trunk/DataAccess-ejb/ejbModule/hu/bme/dtt/conferenceportal/entity/Tag.java
@@ -83,4 +83,48 @@ public class Tag implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+		result = prime * result
+				+ ((getName() == null) ? 0 : getName().hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Tag other = (Tag) obj;
+
+		if (getName() == null) {
+			if (other.getName() != null) {
+				return false;
+			}
+		} else if (!getName().equals(other.getName())) {
+			return false;
+		}
+		return true;
+	}
+
 }
