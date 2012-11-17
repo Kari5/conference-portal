dcd9bbfec21d38e569e27dfaf7b82dc23fd427d4
shazak.tt@gmail.com <shazak.tt@gmail.com@3c564a0e-377f-4616-955d-181d525d466c>
2012-11-17 20:51:15 +0000

    Authentikáció az új implementációval.

--- trunk/ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
+++ trunk/ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
@@ -1,53 +1,29 @@
 package hu.bme.dtt.conferenceportal.session;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-import hu.bme.dtt.conferenceportal.dao.UserDao;
-import hu.bme.dtt.conferenceportal.entity.User;
-
 import org.jboss.logging.Logger;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.management.IdentityManager;
 
 @Name("authenticator")
-public class Authenticator
-{
-    private static final Logger LOGGER = Logger.getLogger(Authenticator.class);
+public class Authenticator {
+	private static final Logger LOGGER = Logger.getLogger(Authenticator.class);
 
-    @In Identity identity;
-    @In Credentials credentials;
-    @In IdentityManager identityManager;
-    
-    public boolean authenticate()
-    {
-    	LOGGER.info("authenticating " + credentials.getUsername());
-        
-    	UserDao userDao;
-    	try {
-			userDao=InitialContext.doLookup("ConferencePortal-ear/userDao/local");
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			LOGGER.info("Belsõ hiba!");
-			return false;
+	@In
+	Identity identity;
+	@In
+	IdentityManager identityManager;
+
+	public boolean authenticate() {
+		LOGGER.info("authenticating " + identity.getCredentials().getUsername());
+
+		if (identityManager.authenticate(identity.getCredentials().getUsername(), identity
+				.getCredentials().getPassword())) {
+			LOGGER.info("authentication successful");
+			return true;
 		}
-    	
-        if ("admin".equals(credentials.getUsername()))
-        {
-            identity.addRole("admin");
-            return true;
-        }
-        
-        if(identityManager.authenticate(credentials.getUsername(), credentials.getPassword())){
-        	identity.addRole("admin");
-        	return true;
-        }
-        
-        return false;
-    }
-    
+		LOGGER.info("authentication failed");
+		return false;
+	}
 }
