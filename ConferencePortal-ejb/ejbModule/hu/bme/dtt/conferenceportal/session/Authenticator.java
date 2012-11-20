0452fc07e0f0d8dcf517b31531c5cf8948a4a6ea
shazak.tt <shazak.tt@gmail.com>
2012-11-20 19:10:23 +0000

    credentials objektumba csak sikeres authentikáció után kerül be a felhasználónév jelszó

--- ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
+++ ConferencePortal-ejb/ejbModule/hu/bme/dtt/conferenceportal/session/Authenticator.java
@@ -6,24 +6,80 @@ import org.jboss.seam.annotations.Name;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.management.IdentityManager;
 
+/**
+ * Komponens felhasználó authentikálásra.
+ */
 @Name("authenticator")
 public class Authenticator {
+	/**
+	 * Naplózáshoz.
+	 */
 	private static final Logger LOGGER = Logger.getLogger(Authenticator.class);
-
+	/**
+	 * Objektum amelyben eltároljuk a session számára a felhasználónevet és
+	 * jelszót.
+	 */
 	@In
-	Identity identity;
+	private Identity identity;
+	/**
+	 * Objektum authentikáció elvégzésére.
+	 */
 	@In
-	IdentityManager identityManager;
+	private IdentityManager identityManager;
+	/**
+	 * A fehasználónév.
+	 */
+	private String userName;
+	/**
+	 * A jelszó.
+	 */
+	private String password;
 
+	/**
+	 * Az authentikációt elvégzõ függvény.
+	 * 
+	 * @return sikeresség
+	 */
 	public boolean authenticate() {
-		LOGGER.info("authenticating " + identity.getCredentials().getUsername());
+		LOGGER.info("authenticating " + userName);
 
-		if (identityManager.authenticate(identity.getCredentials().getUsername(), identity
-				.getCredentials().getPassword())) {
+		if (identityManager.authenticate(userName, password)) {
 			LOGGER.info("authentication successful");
+			identity.getCredentials().setUsername(userName);
+			identity.getCredentials().setPassword(password);
 			return true;
 		}
 		LOGGER.info("authentication failed");
 		return false;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
 	}
 }
