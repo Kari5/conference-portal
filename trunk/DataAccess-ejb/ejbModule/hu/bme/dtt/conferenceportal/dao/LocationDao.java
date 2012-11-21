27ce83c007f379dca91200476021a4bf32c5ca8d
shazak.tt@gmail.com <shazak.tt@gmail.com@3c564a0e-377f-4616-955d-181d525d466c>
2012-11-21 20:45:50 +0000

    Location entitáshoz DAO

---
+++ trunk/DataAccess-ejb/ejbModule/hu/bme/dtt/conferenceportal/dao/LocationDao.java
@@ -0,0 +1,13 @@
+package hu.bme.dtt.conferenceportal.dao;
+
+import hu.bme.dtt.conferenceportal.entity.Location;
+import hu.futurion.mt.dao.GenericDao;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface LocationDao extends GenericDao<Location> {
+	List<Location> getAllLocation();
+}
---
+++ trunk/DataAccess-ejb/ejbModule/hu/bme/dtt/conferenceportal/dao/LocationDaoImpl.java
@@ -0,0 +1,28 @@
+package hu.bme.dtt.conferenceportal.dao;
+
+import hu.bme.dtt.conferenceportal.entity.Location;
+import hu.futurion.mt.dao.GenericDaoImpl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
+
+public class LocationDaoImpl extends GenericDaoImpl<Location> implements LocationDao {
+	private static final Logger LOGGER = Logger.getLogger(ArticleDaoImpl.class);
+	@PersistenceContext(name = "ConferencePortal")
+	private EntityManager entityManager;
+
+	@Override
+	public List<Location> getAllLocation() {
+		return (List<Location>) executeQueryMultipleResult("FROM Location");
+	}
+
+	@Override
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+}
